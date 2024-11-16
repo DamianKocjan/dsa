c10ab1f3695ab671d23ce3ad6351d03b05a5be9e
Damian Kocjan <DamianoKPL@wp.pl>
2024-11-16 15:19:28 +0100

    bubble sort

---
+++ sort-array/bubble-sort/README.md
@@ -0,0 +1,27 @@
+# Bubble Sort
+
+## Time Complexity:
+
+Time Complexity: O(n^2)
+Auxiliary Space: O(1)
+
+## Advantages of Bubble Sort:
+
+- Bubble sort is easy to understand and implement.
+- It does not require any additional memory space.
+- It is a stable sorting algorithm, meaning that elements with the same key value maintain their relative order in the sorted output.
+
+## Disadvantages of Bubble Sort:
+
+- Bubble sort has a time complexity of O(n^2) which makes it very slow for large data sets.
+- Bubble sort is a comparison-based sorting algorithm, which means that it requires a comparison operator to determine the relative order of elements in the input data set. It can limit the efficiency of the algorithm in certain cases.
+
+## Pseudocode:
+
+```
+BubbleSort(A)
+    for i = 0 to A.length - 1
+        for j = 0 to A.length - i - 1
+            if A[j] > A[j + 1]
+                swap(A[j], A[j + 1])
+```
---
+++ sort-array/bubble-sort/main.cpp
@@ -0,0 +1,59 @@
+#include <algorithm>
+#include <iostream>
+#include <random>
+
+#define SIZE 100
+
+void swap(int &a, int &b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void bubbleSort(int arr[]) {
+    for (int i = 0; i < SIZE - 1; i++) {
+        for (int j = 0; j < SIZE - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+int randomInt(int min, int max) {
+    return rand() % (max - min + 1) + min;
+}
+
+void generateRandomArray(int arr[], int size) {
+    for (int i = 0; i < size; ++i) {
+        arr[i] = randomInt(0, 100);;
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+
+int main() {
+    int arr[SIZE];
+
+    srand(time(0));
+
+    generateRandomArray(arr, SIZE);
+
+    std::cout << "Unsorted Array: " << std::endl;
+
+    printArray(arr, SIZE);
+
+    bubbleSort(arr);
+
+    std::cout << "Sorted Array: " << std::endl;
+
+    printArray(arr, SIZE);
+
+    return 0;
+}
